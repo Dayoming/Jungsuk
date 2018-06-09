6f9f895ea6b9e5699ad21600f1ae528f2b41fbdc
Dayoming <jdayoung1204@naver.com>
2018-06-10 01:06:28 +0900

    예제 8-4 수정

--- src/com/javastudy/book/chapter8/Exercise8_4.java
+++ src/com/javastudy/book/chapter8/Exercise8_4.java
@@ -6,6 +6,17 @@ package com.javastudy.book.chapter8;
    class InvalidNumberException extends NumberException { }
    class NotANumberException extends NumberException { }
 
+   a. try { method(); } catch(Exception e) {}
+   b. try { method(); } catch(NumberException e) {} catch(Exception e) {}
+   c. try { method(); } catch(Exception e) {} catch(NumberException e) {}
+   d. try { method(); } catch(InvalidNumberException e) {} catch (NotANumberException e) {}
+   e. try { method(); } catch(NumberException e) {}
+   f. try { method(); } catch(RuntimeException e) {}
+
+   정답 : c
+   Exception은 모든 예외를 다 처리할 수 있으므로 Exception의 하위 예외들보다
+   위에 있으면 안됨(?)
+
  */
 
 public class Exercise8_4 {

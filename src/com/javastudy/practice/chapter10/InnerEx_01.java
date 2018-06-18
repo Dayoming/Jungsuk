bcd8a267d006fafe9b20f7cfe11064d741325006
Dayoming <jdayoung1204@naver.com>
2018-06-18 22:24:04 +0900

    chapter10 내부 클래스 연습 커밋

---
+++ src/com/javastudy/practice/chapter10/InnerEx_01.java
@@ -0,0 +1,31 @@
+package com.javastudy.practice.chapter10;
+
+public class InnerEx_01 {
+    class InstanceInner { // instance 클래스
+        int iv = 100;
+        // static int cv = 100;
+        // Instance 클래스에서 static 변수를 선언할 수 없다.
+        final static int CONST = 100; // final static은 상수이므로 허용
+    }
+
+    static class StaticInner { // static 클래스
+        int iv = 200;
+        static int cv = 200; // static 클래스에서는 가능
+    }
+
+    void myMethod() { // 지역 클래스
+        class LocalInner {
+            int iv = 300;
+            // static int cv = 300;
+            // 불가능
+            final static int CONST = 300;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(InstanceInner.CONST);
+        System.out.println(StaticInner.cv);
+    }
+
+
+}

baf6edec8cd0e27eb6eaa5720299158d6651fcac
Dayoming <jdayoung1204@naver.com>
2018-06-22 10:07:42 +0900

    chapter11 Map 연습

---
+++ src/com/javastudy/practice/chapter11/MapEx_01.java
@@ -0,0 +1,77 @@
+package com.javastudy.practice.chapter11;
+
+/*
+키와 값을 하나의 쌍으로 묶어서 저장하는 컬렉션
+기존에 저장된 데이터와 중복된 키와 값을 저장하면 기존의 값은 없어지고 마지막에 저장된 값이 남게 됨
+Hashtable, HashMap, LinkedHashMap, SortedMap, TreeMap
+*/
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class MapEx_01 {
+    public static void main(String[] args) {
+        Map map = new Map() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object key) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object value) {
+                return false;
+            }
+
+            @Override
+            public Object get(Object key) {
+                return null;
+            }
+
+            @Override
+            public Object put(Object key, Object value) {
+                return null;
+            }
+
+            @Override
+            public Object remove(Object key) {
+                return null;
+            }
+
+            @Override
+            public void putAll(Map m) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Set keySet() {
+                return null;
+            }
+
+            @Override
+            public Collection values() {
+                return null;
+            }
+
+            @Override
+            public Set<Entry> entrySet() {
+                return null;
+            }
+        }
+    }
+}

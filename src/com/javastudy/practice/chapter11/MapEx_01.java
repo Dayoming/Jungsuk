55c748e3b03b041ec435026dc060ec8ee82b8408
Dayoming <jdayoung1204@naver.com>
2018-06-22 10:36:15 +0900

    chapter11 Map 연습

--- src/com/javastudy/practice/chapter11/MapEx_01.java
+++ src/com/javastudy/practice/chapter11/MapEx_01.java
@@ -13,6 +13,7 @@ import java.util.Set;
 public class MapEx_01 {
     public static void main(String[] args) {
         Map map = new Map() {
+
             @Override
             public int size() {
                 return 0;
@@ -27,51 +28,60 @@ public class MapEx_01 {
             public boolean containsKey(Object key) {
                 return false;
             }
+            // 지정된 key객체와 일치하는 Map의 key객체가 있는지 확인
 
             @Override
             public boolean containsValue(Object value) {
                 return false;
             }
+            // 지정된 value객체와 일치하는 Map의 value객체가 있는지 확인
 
             @Override
             public Object get(Object key) {
                 return null;
             }
+            // 지정한 key객체와 대응하는 value객체 반환
 
             @Override
             public Object put(Object key, Object value) {
                 return null;
             }
+            // Map에 value객체를 key객체에 연결하여 저장
 
             @Override
             public Object remove(Object key) {
                 return null;
             }
+            // 지정한 key객체와 key-value객체 삭제
 
             @Override
             public void putAll(Map m) {
-
             }
+            // 지정한 Map의 모든 key-value쌍 추가
 
             @Override
             public void clear() {
-
             }
 
             @Override
             public Set keySet() {
                 return null;
             }
+            // Map에 저장된 모든 key객체 반환
+            // 반환 타입이 Set이므로 중복 불가
 
             @Override
             public Collection values() {
                 return null;
             }
+            // Map에 저장된 모든 value객체 반환
+            // 반환 타입이 Collection이므로 중복 허용
 
             @Override
             public Set<Entry> entrySet() {
                 return null;
             }
-        }
+            // Map에 저장되어 있는 key-value 쌍을 Map.Entry타입의 객체로 저장한 Set으로 반환
+        };
     }
 }

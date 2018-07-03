41c4de0c9627ae49e641482689580a5d8cdf7699
Dayoming <jdayoung1204@naver.com>
2018-07-03 19:56:20 +0900

    연습문제 11-10 커밋

--- src/com/javastudy/book/chapter11/Exercise11_10.java
+++ src/com/javastudy/book/chapter11/Exercise11_10.java
@@ -1,8 +1,10 @@
 package com.javastudy.book.chapter11;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+/* 다음 예제는 이용해서 빙고판은 1~30 사이의 숫자들로 만든 것인데, 숫자들의 위치가
+잘 섞이지 않는다는 문제가 있다. 이러한 문제가 발생하는 이유와 이 문제를 개선
+하기 위한 방법을 설명하고, 이를 개선한 새로운 코드를 작성하시오. */
+
+import java.util.*;
 
 public class Exercise11_10 {
     public static void main(String[] args) {
@@ -13,7 +15,10 @@ public class Exercise11_10 {
             set.add((int)(Math.random() * 30) + 1 + "");
         }
 
-        Iterator it = set.iterator();
+        ArrayList list = new ArrayList(set);
+        Collections.shuffle(list);
+
+        Iterator it = list.iterator();
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {

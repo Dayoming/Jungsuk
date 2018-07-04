772895b1c5d2f82d945672876b42d833a8c2f6cb
Dayoming <jdayoung1204@naver.com>
2018-07-04 23:19:39 +0900

    연습문제 11-13 문제 추가

--- src/com/javastudy/book/chapter11/Exercise11_13.java
+++ src/com/javastudy/book/chapter11/Exercise11_13.java
@@ -1,5 +1,10 @@
 package com.javastudy.book.chapter11;
 
+/* 다음 코드는 문제 11-12를 발전시킨 것으로 각 Player들의 점수를 계산하고,
+* 점수가 제일 높은 사람을 출력하는 코드이다. TreeMap의 정렬기준을 점수가 제일 높은 사람
+* 부터 내림차순이 되도록 아래의 코드를 완성하시오.
+* 단, 동점자 처리는 하지 않는다. */
+
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.TreeMap;
@@ -21,7 +26,12 @@ public class Exercise11_13 {
         TreeMap rank = new TreeMap(new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
-                return 0;
+                if (o1 instanceof SutdaDeck2 && o2 instanceof SutdaDeck2) {
+                    Player player = (Player)o1;
+                    Player player2 = (Player)o2;
+                    return player2.point - player.point;
+                }
+                return -1;
             }
         });
 

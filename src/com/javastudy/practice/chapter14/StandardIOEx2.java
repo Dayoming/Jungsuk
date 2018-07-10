c571a4fbdcbe0a66147de7c180f4efa9e74dc5a4
Dayoming <jdayoung1204@naver.com>
2018-07-10 23:39:01 +0900

    chapter14 System.err 연습

---
+++ src/com/javastudy/practice/chapter14/StandardIOEx2.java
@@ -0,0 +1,8 @@
+package com.javastudy.practice.chapter14;
+
+public class StandardIOEx2 {
+    public static void main(String[] args) {
+        System.out.println("out : Hello World!");
+        System.err.println("err : Hello World!");
+    }
+}
